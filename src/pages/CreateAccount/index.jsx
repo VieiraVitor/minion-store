bf2eb7b04181c08252c4ffa3bbc69f16aa4e444e
VieiraVitor <vieirachp@hotmail.com>
2020-08-09 18:51:03 -0300

    feat: adiciona página de cadastro

--- src/pages/CreateAccount/index.jsx
+++ src/pages/CreateAccount/index.jsx
@@ -2,32 +2,38 @@ import React from 'react';
 import Input from '../../components/Input';
 
 import '../../assets/styles/global.css'
-import './style.css';
+import './style.css'
 
-function Login() {
+function CreateAccount() {
     return (
-        <div id="page-login-form" className="container">
+        <div id="page-create-account-form" className="container">
             <main>
                 <form>
-                    <h1>Sign In</h1>
+                    <h1>Create Account</h1>
                     <Input
                         type="text"
-                        placeholder="Login"
-                        label="Login"
+                        placeholder="Name"
+                    />
+                    <Input
+                        type="email"
+                        placeholder="Email"
                     />
                     <Input
                         type="password"
                         placeholder="Password"
-                        label="Password"
+                    />
+                    <Input
+                        type="password"
+                        placeholder="Confirm Password"
                     />
                     <footer>
                         <button type="submit">
-                            Sign In
+                            Create Account
                         </button>
                         <p>
-                            Don't have a account ?
-                            <a href="/new-account">
-                                Sign Up
+                           Already have an account ?
+                            <a href="/">
+                                Sign In
                             </a>
                         </p>
                     </footer>
@@ -37,4 +43,4 @@ function Login() {
     )
 }
 
-export default Login;
+export default CreateAccount;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import CreateAccount from './pages/CreateAccount';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact component={Login} />  
+            <Route path="/new-account" exact component={CreateAccount} />  
         </BrowserRouter>
     );
 }
