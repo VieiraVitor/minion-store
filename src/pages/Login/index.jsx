0702b0edab446cd0c637dd3e6b756acadcac49c9
VieiraVitor <vieirachp@hotmail.com>
2020-08-09 18:23:35 -0300

    feat: adiciona página de Login

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Input from '../../components/Input';
+
+import '../../assets/styles/global.css'
+import './style.css';
+
+function Login() {
+    return (
+        <div id="page-login-form" className="container">
+            <main>
+                <form>
+                    <h1>Sign In</h1>
+                    <Input
+                        type="text"
+                        placeholder="Login"
+                        label="Login"
+                    />
+                    <Input
+                        type="password"
+                        placeholder="Password"
+                        label="Password"
+                    />
+                    <footer>
+                        <button type="submit">
+                            Sign In
+                        </button>
+                        <p>
+                            Don't have a account ?
+                            <a href="/">
+                                Sign Up
+                            </a>
+                        </p>
+                    </footer>
+                </form>
+            </main>
+        </div>
+    )
+}
+
+export default Login;
