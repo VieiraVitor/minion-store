a6517a6f9abff338ddb05e35da8c953482790b86
VieiraVitor <vieirachp@hotmail.com>
2020-08-13 00:08:06 -0300

    feat: adiciona rota create da API

---
+++ minion-store-api/create.js
@@ -0,0 +1,50 @@
+import * as uuid from "uuid";
+import AWS from "aws-sdk";
+
+// Inicializa conexão com o banco
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      reservationId: uuid.v1(),
+      name: data.name,
+      number: data.number,
+      email: data.email,
+      minions: data.minions,
+      createdAt: Date.now()
+    }
+  };
+
+  dynamoDb.put(params, (error, data) => {
+    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    };
+
+    // Return status code 500 on error
+    if (error) {
+      const response = {
+        statusCode: 500,
+        headers: headers,
+        body: JSON.stringify({ status: false })
+      };
+      callback(null, response);
+      return;
+    }
+
+    // Return status code 200 and the newly created item
+    const response = {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify(params.Item)
+    };
+    callback(null, response);
+  });
+}
