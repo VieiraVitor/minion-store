fbf359b53109e91f95ec0b87364336a0f2aef4c0
VieiraVitor <vieirachp@hotmail.com>
2020-08-13 21:04:43 -0300

    feat: adiciona função list Adiciona get list das reservas feitas por um usuário

--- minion-store-api/libs/dynamodb-lib.js
+++ minion-store-api/libs/dynamodb-lib.js
@@ -3,6 +3,12 @@ import AWS from "aws-sdk";
 const client = new AWS.DynamoDB.DocumentClient();
 
 export default {
+  // recupera a reserva especifica de um usuário
   get   : (params) => client.get(params).promise(),
+
+  // cria uma reserva
   put   : (params) => client.put(params).promise(),
+  
+  // recupera todas as reservas feitas por usuário
+  query   : (params) => client.query(params).promise(),
 };
