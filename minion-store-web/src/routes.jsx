5a87a468459304d52f39a96933886766b10b22cb
VieiraVitor <vieirachp@hotmail.com>
2020-08-13 23:03:52 -0300

    feat: adiciona component NotFound

--- minion-store-web/src/routes.jsx
+++ minion-store-web/src/routes.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import CreateAccount from './pages/CreateAccount';
 import Landing from './pages/Landing';
+import NotFound from './components/NotFound';
 
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Login} />  
-            <Route path="/new-account" exact component={CreateAccount} />  
-            <Route path="/landing" exact component={Landing} />  
+            <Switch>
+                <Route path="/" exact component={Login} />
+                <Route path="/new-account" exact component={CreateAccount} />
+                <Route path="/landing" exact component={Landing} />
+                <Route component={NotFound} />
+            </Switch>
         </BrowserRouter>
     );
 }
