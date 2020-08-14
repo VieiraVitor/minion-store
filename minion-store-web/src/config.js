d3bc8b1bbd8feeb8ae3c4736c27932fff1be383e
VieiraVitor <vieirachp@hotmail.com>
2020-08-13 23:28:07 -0300

    feat: adiciona AWS Amplify

---
+++ minion-store-web/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "reservations-upload"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://t5sdmw89i2.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_eDsMQZjRJ",
+      APP_CLIENT_ID: "7cmo7m090hrecbne778i24bsh",
+      IDENTITY_POOL_ID: "us-east-1:1c21bcb0-b6f1-4ed3-8290-e8800940f2ea"
+    }
+  };
