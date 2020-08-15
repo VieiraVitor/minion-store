9a530467d407c6de40e366e9510254e53943a024
VieiraVitor <vieirachp@hotmail.com>
2020-08-14 21:00:34 -0300

    feat: adiciona carregamento do estado da sessão

--- minion-store-web/src/App.js
+++ minion-store-web/src/App.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Routes from './routes';
 import { AppContext } from './libs/contextLib';
+import { Auth } from 'aws-amplify';
 
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad(){
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if( e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
+
   return (
+    !isAuthenticating &&
     <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
       <Routes />
     </AppContext.Provider>
