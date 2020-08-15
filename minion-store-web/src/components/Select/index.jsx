3b6b9933c63a71de291cbb81c637c6a69424b4dd
VieiraVitor <vieirachp@hotmail.com>
2020-08-15 13:29:22 -0300

    fix: atribui defaultValue no component Select

--- minion-store-web/src/components/Select/index.jsx
+++ minion-store-web/src/components/Select/index.jsx
@@ -5,7 +5,7 @@ function Select({name, label, options, ...props}) {
     return (
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-            <select value="" id={name} {...props}>
+            <select defaultValue="" id={name} {...props}>
                 <option value="" disabled hidden>Selecione uma opção</option>
                 {options.map(option => {
                     return <option key={option.value} value={option.value}>{option.label}</option>
